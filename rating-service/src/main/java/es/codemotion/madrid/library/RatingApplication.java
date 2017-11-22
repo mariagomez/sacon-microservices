a5f8a471128febe71c3b787b74c0513d055be250
Maria Gomez <mgomez@thoughtworks.com>
2017-11-22 18:13:04 +0100

    Added new rating service

---
+++ rating-service/src/main/java/es/codemotion/madrid/library/RatingApplication.java
@@ -0,0 +1,12 @@
+package es.codemotion.madrid.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingApplication.class, args);
+	}
+}
